b72de22ddbf86bda84563901349e219b0fe3f788
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-26 23:02:45 +0100

    Everything about stream. Which streams do we have and hwo to create a file write/read

---
+++ File/Files/File/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+// dont forget to close buffers and filestreams.
+// otherwise it wont return text from file and save it to file.
+
+namespace Fileq
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = "./text.txt";
+
+            using(FileStream file = File.Open(path, FileMode.OpenOrCreate))
+            {
+                if (File.Exists(path)) Console.WriteLine("Exists");
+                else return;
+
+                StreamWriter write = new StreamWriter(file);
+                write.WriteLine("Heyy");
+                write.WriteLine("Heyy twice");
+                write.Close();
+                file.Close();
+            }
+
+            using (FileStream f = File.Open(path, FileMode.OpenOrCreate))
+            {
+                StreamReader read = new StreamReader(f);
+                Console.WriteLine(read.ReadToEnd());
+                read.Close();
+                f.Close();
+            }
+
+        }
+    }
+}
---
+++ Stream_IO/StreamIO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/**
+ * 
+ * SubStreams that inherits from Stream class.
+ * 
+ * FiliStream, MemoryStream, BufferedStream, NetworkStream,
+ * PipeStream, CryptoStream
+ * 
+ * StreamReader, StreamWriter, BinaryReader, BinaryWriter
+ * 
+ **/
+
+
+namespace StreamIO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
