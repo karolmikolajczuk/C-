7f92da91f0d0351519d41540b23e3f5aebaa15df
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-26 19:49:10 +0100

    Nullable, Default Values, Pointers are not ended cuz of IDE which doesnt allow unsafe blocks coding - telling me pointer is an error >.<

---
+++ Nullable/Nullable/Nullable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+   /*  
+    * 
+    * Example of use a nullable "structure"
+    * 
+    */
+
+namespace Nullable
+{
+    class ExampleClass 
+    {
+        public int j;
+        public Nullable<int> i;
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // set an integer variable a NULL value
+            Nullable<int> i = null;
+
+            if (i.HasValue) Console.WriteLine(i.Value);
+            else Console.WriteLine("NULL");
+
+            Nullable<int> j = null;
+            Console.WriteLine(j.GetValueOrDefault());
+
+            int? k = null;
+            Console.WriteLine(k.GetValueOrDefault());
+
+            int? l = null;
+            int m = l ?? 0;
+            Console.WriteLine(m);
+
+            ExampleClass obj = new ExampleClass();
+            if (obj.i == null) Console.WriteLine("Jest Nullem");
+            if (obj.j == null) Console.WriteLine("Jest Nullem");
+            else Console.WriteLine(obj.j); // what is a default value
+        }
+    }
+}
---
+++ Pointers/Pointers/Pointers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pointers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int a = 5;
+            unsafe
+            {
+                fixed(int* ptr_a = &a)
+                {
+                    
+                }
+            }
+
+        }
+    }
+}
