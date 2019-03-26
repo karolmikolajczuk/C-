4ac8969647231f8dd8187a468535d2cc8b223278
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-26 22:03:50 +0100

    How to extend basic (global) methods for basic data types

---
+++ Extension_Method/ExtensionMethods/ExtensionMethods/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static bool isGreater(this int i, int value)
+        {
+            return i > value;
+        }
+
+        public static bool isLonger(this string text, string value)
+        {
+            return text.Length > value.Length;
+        }
+    }
+}
---
+++ Extension_Method/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number = 10;
+            if (number.isGreater(9)) Console.WriteLine(true);
+            if (number.isGreater(10)) Console.WriteLine(false);
+
+            string text = "Hey";
+            if (text.isLonger("Heyy")) Console.WriteLine(false);
+            if (text.isLonger("He")) Console.WriteLine(true);
+        }
+    }
+}
