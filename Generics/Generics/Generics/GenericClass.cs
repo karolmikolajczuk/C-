689c99f10d30983a67f155e2a2293559c33465dc
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-26 16:55:07 +0100

    Add a miniproject or rather an exercise of Generics things. Few keywords and a basic thing

---
+++ Generics/Generics/Generics/GenericClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Generics
+{
+    public class GenericClass<T>
+    {
+        public int year { get; set; }
+        private string name { get; set; }
+        private T birth_date { get; set; }
+
+        public GenericClass(int year, string name, T birth_date)
+        {
+            this.year = year;
+            this.name = name;
+            this.birth_date = birth_date;
+        }
+
+        static public void Print(GenericClass<T> o)
+        {
+            Console.WriteLine("Year: {0}\nName: {1}\nBirth Date: {2}", o.year, o.name, o.birth_date);
+        }
+    }
+
+    // constraint for generic parameter 
+    // T has to be a class type.
+
+    public class GenericClass2<A> where A: class
+    {
+        static public void Printer(A param)
+        {
+            Console.WriteLine(typeof(A).ToString());
+        }
+    }
+}
---
+++ Generics/Generics/Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GenericClass<string> obj = new GenericClass<string>(21, "Karol", "19-07-1997");
+            GenericClass<string>.Print(obj);
+            Console.WriteLine("Get year: " + obj.year.ToString());
+
+            obj.year = 23;
+            Console.WriteLine("Set year: " + obj.year);
+
+            Console.WriteLine("Printing a second class thing");
+            Console.WriteLine("Printing what object was sent through parameter");
+            Console.WriteLine("Used 'where' keyword.");
+            GenericClass2<GenericClass<string>>.Printer(obj);
+        }
+    }
+}
