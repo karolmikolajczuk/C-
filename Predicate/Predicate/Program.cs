bdc8ff8d4e7891300833514908c813498b81ca61
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-26 21:27:37 +0100

    Add Predicate delegate.

---
+++ Predicate/Predicate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Predicate
+{
+    class MainClass
+    {
+        private static bool isStringUpper(string text)
+        {
+            return text.Equals(text.ToUpper());
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Predicate<string> checkUpper = isStringUpper;
+            Console.WriteLine(checkUpper("Hey Babe"));
+            Console.WriteLine(checkUpper("YES"));
+            Console.WriteLine(checkUpper("no"));
+            Console.WriteLine(checkUpper(".")); // it will give true, cuz after upper method dot is still a dot (no bigger dot or something)
+        }
+    }
+}
