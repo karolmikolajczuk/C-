ece1c9fec6836eaa5e3bb54112ef96f7109ad3d2
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-27 14:26:15 +0100

    StringBuilder - a better thing to work with when we need a String and manipulate them

---
+++ StringBuilder/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+// StringBuilder doesnt create another object when modyfing an object like
+// string class does.
+
+// StringBuilder saves memory
+
+
+namespace StringBuilderq
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // create objects
+            string a = "hey";
+            StringBuilder sb = new StringBuilder("heyy");
+
+            // print them
+            Console.WriteLine(a);
+            Console.WriteLine(sb);
+
+            // modify, where STRING will have another memory address
+            // and stringbuilder will have the same one
+            a += "HEEEEEEY";
+            sb.Append("HHHHHHHH");
+
+            // print values, where each one will be a correctly modyfied
+            // no costly conversion and stuff
+            Console.WriteLine(a);
+            Console.WriteLine(sb);
+        }
+    }
+}
