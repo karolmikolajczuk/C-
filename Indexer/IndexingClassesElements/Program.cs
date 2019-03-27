fa7becdc372a37672ef1586c96f14d1c463ce5ad
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-27 14:49:07 +0100

    Show how Indexing custom class

---
+++ Indexer/IndexingClassesElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IndexingClassesElements
+{
+    class MyFriendsNames 
+    {
+        private string[] friendsList = new string[10];
+
+
+        // indexing 
+        public string this[int index]
+        {
+            // give me data about specific person from list
+            get 
+            {
+                if (index < 0 || index > 9) throw new IndexOutOfRangeException("Just 10 friends, no more");
+
+                return friendsList[index];
+            }
+            // set value into array of friendslist
+            set
+            {
+                if (index < 0 || index > 9) throw new IndexOutOfRangeException("Just 10 friends, no more");
+
+                friendsList[index] = value;
+            }
+        }
+    }
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyFriendsNames JimmysFriends = new MyFriendsNames();
+            JimmysFriends[0] = "Tom";
+            JimmysFriends[1] = "Kobe";
+
+            Console.WriteLine(JimmysFriends[0]);
+            Console.WriteLine(JimmysFriends[1]);
+        }
+    }
+}
