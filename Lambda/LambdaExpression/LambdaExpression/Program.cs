4c7ddec7c3d55b87e844b7af07384e9370de89b5
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-27 14:41:24 +0100

    Show how lambda expressions works

---
+++ Lambda/LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// lambda is a shorter way to write a function
+// lambda doesnt have constraints about have arguments or not.
+// lambda can have multiple lines
+
+
+namespace LambdaExpression
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Func<int, int, int> Delegate = (x, y) => x + y;
+            Console.WriteLine(Delegate(10, 10));
+
+            Action<int> act = (x) =>
+            {
+                int a = 222;
+                Console.WriteLine(x);
+                Console.WriteLine(a);
+            };
+            act(99);
+        }
+    }
+}
